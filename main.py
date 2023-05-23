1c883f41df24681f45576fe4f203d41c034e36ea
Cha1552164 <chamithgithub>
2023-05-23 13:41:57 +0530

    some button add

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import tkinter as tk
-
 calculation = ""
+
 def add_to_calculation(symbol):
-    pass
+    global calculation
+    calculation +=str(symbol)
+    text_result.delete(1.0,"end")
+    text_result.insert(1.0,calculation)
+
 def evoluatr_calculation():
-    pass
+    global calculation
+
+
+
 def clear_field():
-    pass
+    global calculation
+    calculation=""
+    text_result.delete(1.0,"end")    
+
 
 root =tk.Tk()
 root.geometry("500x375")
@@ -14,4 +24,15 @@ root.title("calculator")
 
 text_result =tk.Text(root, height=2,width=30,font=("Arial",24))
 text_result.grid(columnspan=5)
+
+btn_1=tk.Button(root,text="1",command=lambda:add_to_calculation(1),width=5,font=("Arial",14))
+btn_1.grid(row=1,column=1)
+
+btn_2=tk.Button(root,text="2",command=lambda:add_to_calculation(2),width=5,font=("Arial",14))
+btn_2.grid(row=1,column=2)
+
+btn_3=tk.Button(root,text="3",command=lambda:add_to_calculation(3),width=5,font=("Arial",14))
+btn_3.grid(row=1,column=3)
+
+
 root.mainloop()
