553eae9d01bc1b2bc4430e0cec257ade81558a42
Cha1552164 <chamithgithub>
2023-05-23 14:37:19 +0530

    button add

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+
 calculation = ""
 
 def add_to_calculation(symbol):
@@ -7,15 +8,22 @@ def add_to_calculation(symbol):
     text_result.delete(1.0,"end")
     text_result.insert(1.0,calculation)
 
-def evoluatr_calculation():
+def evoluate_calculation():
     global calculation
-
+# try:
+#     calculation =str(eval(calculation))
+#     text_result.delete(1.0,"end")
+#     text_result.insert(1.0,calculation)
+    
+# except:
+#       clear_field()
+#       text_result.insert(1.0,"Erorr")
 
 
 def clear_field():
     global calculation
     calculation=""
-    text_result.delete(1.0,"end")    
+    text_result.delete(1.0,"end")
 
 
 root =tk.Tk()
@@ -33,6 +41,24 @@ btn_2.grid(row=1,column=2)
 
 btn_3=tk.Button(root,text="3",command=lambda:add_to_calculation(3),width=5,font=("Arial",14))
 btn_3.grid(row=1,column=3)
+
+btn_4=tk.Button(root,text="4",command=lambda:add_to_calculation(4),width=5,font=("Arial",14))
+btn_4.grid(row=2,column=1)
+
+btn_5=tk.Button(root,text="5",command=lambda:add_to_calculation(5),width=5,font=("Arial",14))
+btn_5.grid(row=2,column=2)
+
+btn_6=tk.Button(root,text="6",command=lambda:add_to_calculation(6),width=5,font=("Arial",14))
+btn_6.grid(row=2,column=3)
+
+btn_7=tk.Button(root,text="7",command=lambda:add_to_calculation(7),width=5,font=("Arial",14))
+btn_7.grid(row=3,column=1)
+
+btn_8=tk.Button(root,text="8",command=lambda:add_to_calculation(8),width=5,font=("Arial",14))
+btn_8.grid(row=3,column=2)
+
+btn_9=tk.Button(root,text="9",command=lambda:add_to_calculation(9),width=5,font=("Arial",14))
+btn_9.grid(row=3,column=3)
 
 
 root.mainloop()
