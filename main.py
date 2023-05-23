b4f84c4cd774d3e1df944ff630feec1f3a6754b6
Cha1552164 <chamithgithub>
2023-05-23 12:54:05 +0530

    widow design add

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tkinter as hhhh
+import tkinter as tk
 
 calculation = ""
 def add_to_calculation(symbol):
@@ -8,6 +8,10 @@ def evoluatr_calculation():
 def clear_field():
     pass
 
-root =hhhh.Tk()
-root.geometry("300x275")
+root =tk.Tk()
+root.geometry("500x375")
+root.title("calculator")
+
+text_result =tk.Text(root, height=2,width=30,font=("Arial",24))
+text_result.grid(columnspan=5)
 root.mainloop()
