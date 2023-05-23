188d22b97caa5960cbc84e3607bfc854bce304a8
Cha1552164 <chamithgithub>
2023-05-23 12:35:38 +0530

    create window

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as hhhh
+
+calculation = ""
+def add_to_calculation(symbol):
+    pass
+def evoluatr_calculation():
+    pass
+def clear_field():
+    pass
+
+root =hhhh.Tk()
+root.geometry("300x275")
+root.mainloop()
